ef2c62c52d29a7b667d0923325ab28a6ace28b64
Mariam Zaman-Vaziri <117606811+Mariam-ZV@users.noreply.github.com>
2023-10-09 20:29:03 +0100

    Update title and info

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Mariam's Blog v8",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Mariam",
+		email: "mariam@example.com",
 		url: "https://example.com/about-me/"
 	}
 }
