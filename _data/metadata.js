c7a4ff9d3a40db0f6008d8d903dcefec4130b43d
Mariam-ZV <vaziri_mariam@yahoo.com>
2023-10-09 21:47:46 +0100

    udate title again

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Mariam's Blog v8",
+	title: "Mariam's Blog",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Mariam",
 		email: "mariam@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		url: "https://example.com/about-me/",
+	},
+};
